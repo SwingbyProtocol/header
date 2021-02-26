42333d333d10daafcc256ffdc406e139938d972c
Rubén <ruben@swingby.network>
2021-02-26 16:28:56 +0100

    fix: Theme switcher alignment

--- src/components/ThemeSwitcher/styled.tsx
+++ src/components/ThemeSwitcher/styled.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border: none;
   background: transparent;
   padding: 0;
